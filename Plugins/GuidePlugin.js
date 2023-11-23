0da39b8874c0e68fef53eca3eb0ac349e26e8776
Torther <i@mol.ink>
2023-11-23 17:07:17 +0800

    补上漏提交的文件

--- Plugins/GuidePlugin.js
+++ Plugins/GuidePlugin.js
@@ -1,9 +1,8 @@
-// GuidePlugin.js
 const PluginBase = require('./PluginBase');
 const {default: axios} = require("axios");
 
-const BASEURL = "https://arona.diyigemt.com/api/v1/image";
-const IMAGEURL = "https://arona.cdn.diyigemt.com/image";
+const BASEURL = "https://arona.diyigemt.com/api/v2/image";
+const IMAGEURL = "https://arona.cdn.diyigemt.com/image/s";
 
 class GuidePlugin extends PluginBase {
     constructor() {
@@ -15,19 +14,20 @@ class GuidePlugin extends PluginBase {
 
     async executeCommand(data, command) {
         try {
-            if (command[0] === '/攻略') {
+            if (command[0] === '/攻略' && command[1]) {
                 await this.getTrainerByKeyWord(data, command[1]);
+            } else {
+                await this.sendReferenceMessage(data, "请添加参数，如 /攻略 未花", false);
             }
         } catch (error) {
             console.error(`[ERROR][${this.name}] Error executing command: ${error.message}`);
         }
-
     }
 
     async getTrainerByKeyWord(data, keyword) {
         try {
             const response = await this.fetchTrainerData(keyword);
-            await this.handleTrainerResponse(data, response);
+            await this.handleTrainerResponse(keyword, data, response.data);
         } catch (error) {
             console.error(error);
             await this.sendReferenceMessage(data, "数据源异常，暂时无法使用", false);
@@ -40,13 +40,17 @@ class GuidePlugin extends PluginBase {
         });
     }
 
-    async handleTrainerResponse(data, response) {
-        if (response.data.status === 101) {
-            const msg = `没有找到与 ${response.data.keyword} 对应的信息，是否想要查找：\n - ${response.data.data.map(i => i.name).join('\n - ')}`;
-            await this.sendReferenceMessage(data, msg, false);
-        } else if (response.data.status === 200) {
-            const url = IMAGEURL + response.data.data[0].path;
-            await this.sendReferenceMessage(data, encodeURI(url), true);
+    async handleTrainerResponse(keyword, data, {code, message, data: responseData}) {
+        if (code === 200 && message === "OK") {
+            if (responseData.length === 1) {
+                const url = IMAGEURL + responseData[0].content;
+                await this.sendReferenceMessage(data, encodeURI(url), true);
+            } else {
+                const msg = `没有找到与 ${keyword} 对应的信息，是否想要查找：\n - ${responseData.map(i => i.name).join('\n - ')}`;
+                await this.sendReferenceMessage(data, msg, false);
+            }
+        } else {
+            await this.sendReferenceMessage(data, "数据源异常，暂时无法使用", false);
         }
     }
 }
